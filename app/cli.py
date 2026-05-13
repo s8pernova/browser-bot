33e7ae8c9b9c0dcf3b462c0d7fe3db6272c9b890
Aidan Hoo <achrunaway@gmail.com>
2026-05-13 12:40:53 -0600

    init cli args

--- app/cli.py
+++ app/cli.py
@@ -6,12 +6,34 @@ Contains commands like:
 - init
 """
 
+from argparse import ArgumentParser
 
-def main() -> int:
-    """
-    Main entry point for the CLI.
-    """
-    
+def main(argv: list[str] | None = None) -> int:
+    parser = ArgumentParser(
+        description=""
+    )
+    parser.add_argument(
+        "--run",
+        action="",
+        help="Run a workflow. workflow/ directory is scanned for yaml files."
+    )
+    parser.add_argument(
+        "--validate",
+        action="",
+        help="Validate a workflow yaml file."
+    )
+    parser.add_argument(
+        "--list",
+        action="",
+        help="List all available workflow yaml files."
+    )
+    parser.add_argument(
+        "--init",
+        action="",
+        help="Generate a new workflow yaml file with example steps."
+    )
+    args = parser.parse_args(argv)
+
     return 0
 
 
