d4ceb3896c9c9d1ddca99cf666a35ee652f84f25
Aidan Hoo <achrunaway@gmail.com>
2026-05-13 12:42:43 -0600

    bump

--- app/cli.py
+++ app/cli.py
@@ -14,26 +14,26 @@ def main(argv: list[str] | None = None) -> int:
     )
     parser.add_argument(
         "--run",
-        action="",
+        action="store_true",
         help="Run a workflow. workflow/ directory is scanned for yaml files."
     )
     parser.add_argument(
         "--validate",
-        action="",
+        action="store_true",
         help="Validate a workflow yaml file."
     )
     parser.add_argument(
         "--list",
-        action="",
+        action="store_true",
         help="List all available workflow yaml files."
     )
     parser.add_argument(
         "--init",
-        action="",
+        action="store_true",
         help="Generate a new workflow yaml file with example steps."
     )
     args = parser.parse_args(argv)
-
+    
     return 0
 
 
